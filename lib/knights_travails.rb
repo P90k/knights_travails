396be2520eda483e4e9b94fd08fe75e8bb892bd3
Andreas Jansson <andreas.jansson234@gmail.com>
2023-05-24 15:04:11 +0200

    add #get_node
    method will take an array of two numeric values between 0 and 8 and
    retrieve the node. Each node is supposed to represent one square
    on the chessboard.

--- lib/knights_travails.rb
+++ lib/knights_travails.rb
@@ -7,6 +7,12 @@ class Board
   def print_board
     @board.each {|row| p row}
   end
+
+  def get_node(node_as_array)
+    for node in @board
+      return node if node.place == node_as_array
+    end
+  end
 end
 
 class Node < Board
