447acdcb4b4ea6ef79025e85fc82939c72af559a
Andreas Jansson <andreas.jansson234@gmail.com>
2023-05-27 10:25:35 +0200

    First working version
    - clean up code in #add_possible_knight_moves.
    
    - add method #breadth_first_search that takes two parameters. Starting
      node will be first parameter and the node to look for is second
      parameter. Method uses breadth first search concept.
    
    - add method #knight_moves that takes two parameters, starting_square
      & desired square. Returns shortest path for the knight
      between starting_square and desired_square.

--- lib/create_nodes.rb
+++ lib/create_nodes.rb
@@ -1,3 +1,5 @@
 require_relative 'knights_travails.rb'
 
 board = Board.new()
+
+board.knight_moves([8,1], [1,1])
--- lib/knights_travails.rb
+++ lib/knights_travails.rb
@@ -51,11 +51,43 @@ class Board < Node
   def add_possible_knight_moves()
     change_node do |node|
     x, y = node.place[0], node.place[1]
-    possible_nodes = [[x+2, y+1], [x+2, y-1], [x-2, y+1], [x-2, y-1], [x+1, y+2], [x+1, y-2], [x-1, y-2], [x-1, y+2]]
-    possible_nodes.each do |move|
-      node.knight_moves << move if node_on_board(move)
+    possible_nodes = [[x+1, y+2], [x+1, y-2], [x-1, y-2], [x-1, y+2], [x+2, y+1], [x+2, y-1], [x-2, y+1], [x-2, y-1]]
+    possible_nodes.each {|move| node.knight_moves << move if node_on_board(move) }
     end
   end
+
+  def breadth_first_search(node_as_array, node_to_look_for)
+    nodes_to_visit = []
+    nodes_visited = []
+    get_node(node_as_array).knight_moves.each {|node| nodes_to_visit << node}
+    nodes_visited << node_as_array
+    i = 0
+    while nodes_to_visit.length > 0 do
+      i+=1
+      current_node = nodes_to_visit.shift
+      nodes_visited << current_node
+      return [current_node, nodes_visited.uniq] if current_node == node_to_look_for
+      get_node(current_node).knight_moves.each {|node| nodes_to_visit << node}
+    end
+  end 
+
+  def find_parent(node, array)
+    array.each {|parent| return parent if get_node(parent).knight_moves.include?(node) }
   end
 
+  def knight_moves(starting_square, desired_square)
+    results = breadth_first_search(starting_square, desired_square)
+    node_found, nodes_visited = results[0], results[1]
+    path, check_node = [], node_found
+    path << node_found
+    
+    while check_node != starting_square do
+      parent = find_parent(check_node, nodes_visited)
+      path << parent
+      check_node = parent
+    end
+    puts "You made it in #{path.length - 1} moves! Here's your path: "
+    path.reverse.each {|square| p square}
+  end
+    
 end
